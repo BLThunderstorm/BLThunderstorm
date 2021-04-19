3825cc2ab2da51cb6115e27a5f9c3e22480df5c0
Restyled.io <commits@restyled.io>
2021-04-19 05:56:25 +0000

    Restyled by clang-format

--- frontend/plugins/bl.js
+++ frontend/plugins/bl.js
@@ -1,15 +1,15 @@
 import bljs from 'battlelog.js/dist/bundle.dev.js';
 
-export default function({ params, $bljs, $client, $blclient }, inject){
-    if(!$bljs){
-        $bljs = bljs;
-        inject("bljs", $bljs);
-    }
-    if(!$blclient){
-        $blclient = bljs();
-        inject("blclient", $blclient);
-    }
-     if(params.game && (!$client || $client.game !== params.game)) {
+export default function({params, $bljs, $client, $blclient}, inject) {
+  if (!$bljs) {
+    $bljs = bljs;
+    inject("bljs", $bljs);
+  }
+  if (!$blclient) {
+    $blclient = bljs();
+    inject("blclient", $blclient);
+  }
+  if (params.game && (!$client || $client.game !== params.game)) {
     inject("client", $blclient.game(params.game));
-    }
+  }
 }
