6587690487501fa2b27b67a518c62cbb1ea9db76
Nefomemes <nefothingy@hotmail.com>
2021-04-19 09:01:53 +0700

    Delete utils.js

--- backend/src/utils.js
+++
@@ -1,6 +0,0 @@
-// https://stackoverflow.com/a/52395990/13415044
-module.exports.lnSplit = function lnSplit(str, maxLength){
-  if(str.length <= maxLength)  return str;
-  var parts = str.match(new RegExp(".{1," + maxLength + "}","g"));
-  return parts;
-}
