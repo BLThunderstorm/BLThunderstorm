54e8cf16bd495cdf2727e74b5ef8c935d0a5c6e0
Nefomemes <nefothingy@hotmail.com>
2021-04-19 20:35:02 +0700

    Delete vercel.sh

--- vercel.sh
+++
@@ -1,7 +0,0 @@
-if [[ $VERCEL_GIT_COMMIT_MESSAGE == *"[allow preview]"* || "$VERCEL_ENV" == "production" ]]; then
-  echo "✅ - Build can proceed"
-  exit 0
-else
-  echo "🛑 - Build cancelled"
-  exit 1
-fi
