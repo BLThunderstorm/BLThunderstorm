03596614e3b894b484afcb5bb3af01c25abe0838
Nefomemes <nefothingy@hotmail.com>
2021-04-22 09:32:21 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
     "parser": "@babel/eslint-parser",
+    "parserOptions": {
+        "sourceType": "module",
+    },
     "env": {
         "browser": true,
         "es2021": true
