c985a497cadebb1cd7b4e62a4aa30a66430442bc
Nefomemes <nefothingy@hotmail.com>
2021-06-09 14:58:23 +0700

    fix(frontend): Fix import issues in index.vue and some lint errors

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,18 @@
 
 <template>
+<div></div>
 </template>
 <script lang="ts">
-  import { Component } from "vue-class-component";
+  import  Component  from "vue-class-component";
   import Vue from "vue";
   
 @Component({})
 export default class IndexPage extends Vue {
-  async asyncData(ctx){
+  asyncData(ctx){
     
     ctx.$auth.loggedIn ? ctx.redirect('/dashboard') : ctx.redirect('/about');
     
     return {};
     
-}
+}}
 </script>
