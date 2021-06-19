0ab4fc2e5b7996e6bfcf523f8be48a1e9ab565b1
Nefomemes <nefothingy@hotmail.com>
2021-06-19 13:58:26 +0700

    feat(frontend): / redirects to /dashboard if logged in if not to /about

---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import Component from "vue-class-component";
+import Vue from "vue";
+
+@Component({})
+export default class IndexPage extends Vue {
+async asyncData(ctx){
+ctx.$auth?.loggedIn ? ctx.redirect("/dashboard") : ctx.redirect("/about")
+}
+}
+</script>
