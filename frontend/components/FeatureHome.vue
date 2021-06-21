259277f92df55812e18d318698c0c84849855617
Nefomemes <nefothingy@hotmail.com>
2021-06-21 15:46:32 +0700

    fix(frontend): Fix HTML error on FeatureHome component

--- frontend/components/FeatureHome.vue
+++ frontend/components/FeatureHome.vue
@@ -1,24 +1,25 @@
 <template>
-<div class="features">
-	
-<div class="features-icon">
-<v-icon>
-	{{ icon }}
-</v-icon>
-</div>
-<div class="features-content">
-<div class="features-title" v-html="title">
-</div>                                                                                                                                                                                                                                                                                                                                                                              
-<div class="features-text" v-html="content"></div>
-	</div>
-        </div>
-        </div>
-
+  <div class="features">
+    <div class="features-icon">
+      <v-icon>
+        {{ icon }}
+      </v-icon>
+    </div>
+    <div class="features-content">
+      <div class="features-title" v-html="title"></div>
+      <div class="features-text" v-html="content"></div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-props: {title: String, content: String, icon: String, isMaterialIcon: Boolean}
-}
+  props: {
+    title: String,
+    content: String,
+    icon: String,
+    isMaterialIcon: Boolean,
+  },
+};
 </script>
 <style scoped lang="sass">
 .features-title
@@ -26,23 +27,23 @@ props: {title: String, content: String, icon: String, isMaterialIcon: Boolean}
  display: flex
  align-items: center
 
-.features 
+.features
  display: flex
  font-size: 25px !important
  min-width: calc(100% - 80px)
  margin-bottom: 40px
  margin-top: 40px
-   
-.features-content  
+
+.features-content
  display: flex
  flex-direction: column
  margin-left: 5px
-    
-.features-icon 
+
+.features-icon
  display: flex
  align-items: center
  height: 35px
-    
+
 .features-text
  font-size: 20px !important
 </style>
