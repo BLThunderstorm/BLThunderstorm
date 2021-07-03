43faee199a94d250cb36edb4b47acb0edb805fcf
Nefomemes <nefothingy@hotmail.com>
2021-07-04 03:44:19 +0700

    fix(market-garden): Fix Hook error and other errors in /index

--- market-garden/pages/index.tsx
+++ market-garden/pages/index.tsx
@@ -1,19 +1,33 @@
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/client';
+import { getSession } from "next-auth/client";
 
-export const getServerSideProps = async function getServerSideProps(){
-  const [ session, loading ] = useSession();
+export const getServerSideProps: GetServerSideProps =  async (ctx) => {
+    let session = await getSession({req: ctx.req})
 
   if(!session){
     return {
       redirect: "/about",
-      permanent: false
+      permanent: false,
+      props: {
+
+      }
     }
   } else {
     return {
       redirect: "/",
-      permanent: false
+      permanent: false,
+      props: {
+        
+      }
     }
   }
+}
+
+export default function IndexPage(){
+  return (
+    <div>
+      "Redirecting you to your home..."
+    </div>
+  )
 }
 
