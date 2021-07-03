366ee428b28f421a3214cc8aa74985d6eb4f43a6
Nefomemes <nefothingy@hotmail.com>
2021-07-04 04:10:16 +0700

    fix(market-garden): React tells to add props param

--- market-garden/pages/index.tsx
+++ market-garden/pages/index.tsx
@@ -1,5 +1,6 @@
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { getSession } from "next-auth/client";
+import { FunctionComponent } from 'react';
 
 export const getServerSideProps: GetServerSideProps =  async (ctx) => {
     let session = await getSession({req: ctx.req})
@@ -9,7 +10,7 @@ export const getServerSideProps: GetServerSideProps =  async (ctx) => {
       redirect: "/about",
       permanent: false,
       props: {
-
+          auth: true
       }
     }
   } else {
@@ -17,17 +18,18 @@ export const getServerSideProps: GetServerSideProps =  async (ctx) => {
       redirect: "/",
       permanent: false,
       props: {
-        
+       auth: false
       }
     }
   }
 }
 
-export default function IndexPage(){
+const IndexPage: FunctionComponent<{auth: boolean}> = (props) => {
   return (
     <div>
-      "Redirecting you to your home..."
+      `Redirecting you to your home... ${props.auth ?  "You are loggen in." : "You are not logged in." }`
     </div>
   )
 }
 
+export default IndexPage;
