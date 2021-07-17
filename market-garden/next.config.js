e02dad0b44f8b56c6f89ec0eb7105d2a2709bafc
Nefomemes <nefothingy@hotmail.com>
2021-07-17 17:34:25 +0700

    fix(market-garden): Fix loaders

--- market-garden/next.config.js
+++ market-garden/next.config.js
@@ -2,24 +2,15 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
-
+  experimental: {
+    externalDir: true,
+  },
   webpack: (config) => {
 
     config = {
       ...config,
-      module: {
-        rules: [
-          ...(config.modules?.rules || []),
-
-          // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-          { test: /\.tsx?$/, loader: "ts-loader" }
-
-        ]
-      },
       resolve: {
         ...config.resolve,
-        extensions: [...config.resolve?.extensions,
-          ".ts", ".tsx", ".js"],
         alias: {
           ...config.resolve?.alias,
           "~": path.resolve(__dirname)
