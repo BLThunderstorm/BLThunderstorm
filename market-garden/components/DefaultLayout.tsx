c8349d0bc5b8254b53e86302808353ec4aa414e3
Nefomemes <nefothingy@hotmail.com>
2021-07-18 09:06:39 +0700

    refactor(market-garden): Rename layout.tsx to DefaultLayout.tsx

--- market-garden/components/DefaultLayout.tsx
+++ market-garden/components/DefaultLayout.tsx
@@ -1,23 +1,28 @@
 import { FunctionComponent } from "react";
 import { NavigationBar } from "./navigationbar";
 import { Footer } from "./footer";
-export const DefaultLayout: FunctionComponent<{}> = ({ children }) =>{
-    return (<div>
+import type { ReactElement } from "react";
+import "./DefaultLayout.module.scss";
+export const DefaultLayout: FunctionComponent<{ children: ReactElement }> = ({ children }) => {
+
+
+
+  return (<div>
     <div className="bg-picture"></div>
     <NavigationBar />
     <div className="page-content-container">
       <div className="pagecontent">
-        { children }
+        {children}
       </div>
 
       <Footer disclaimers={[
 
         {
-          id:"credit",
-          content:"Made with ♥️ by Nefomemes"
+          id: "credit",
+          content: "Made with ♥️ by Nefomemes"
         }
       ]} />
     </div>
-    </div>
-    );
-    }
+  </div>
+  );
+}
