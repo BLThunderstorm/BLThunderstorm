14a369931f697f65ff7d84968d5dc26edbde381b
Nefomemes <nefothingy@hotmail.com>
2021-07-18 08:54:22 +0700

    fix(market-garden): Fix error in importing JSON file

--- market-garden/util/getPortrait.ts
+++ market-garden/util/getPortrait.ts
@@ -1,20 +1,19 @@
-import * as games from "@nefomemes/blscraps-strings/games.json";
-import * as bf4cdn from "@nefomemes/blscraps-strings/bf4_cdn.json";
+import bf4cdn from "@nefomemes/blscraps-strings/bf4_cdn.json";
 
 export default function getPortrait(
   game: number,
   personaPicture: string
 ): string {
   switch (game) {
-    case games["BF3"]: {
+    case 2: { // Battlefield 3
       return `https://cdn.battlelog.com/bl-cdn/cdnprefix/1323198/public/profile/bf3/soldier/l/${personaPicture}.png`;
     }
-    case games["WARSAW"]: {
+    case 2048: { // Battlefield 4
         // @ts-expect-error
       return `https://d34ymitoc1pg7m.cloudfront.net/bf4/soldier/large/${personaPicture}-${bf4cdn["l_large"][personaPicture]}.png`;
     }
 
-    case games["MOHW"]: {
+    case 4096: { // Medal of Honor: Warfighter
       return `https://cdn.battlelog.com/bl-cdn/cdnprefix/1323198/public/profile/mohw/soldiers/295x350/${
         personaPicture || "default"
       }.png`;
