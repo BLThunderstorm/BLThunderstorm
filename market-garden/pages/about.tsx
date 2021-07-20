e42cc26a5ba684bd80c0aa3c58fdd603dd137833
Nefomemes <nefothingy@hotmail.com>
2021-07-20 20:17:55 +0700

    feat(market-garden): Add styles to About page

--- market-garden/pages/about.tsx
+++ market-garden/pages/about.tsx
@@ -3,25 +3,25 @@ import styles from "./About.module.scss";
 import Head from "next/head";
 export default function AboutPage() {
   return (
-    <DefaultLayout>
-      <div className="about-page">
+
+      <div className={`${styles["about-page"]} about-page`}>
         <Head>
           <title>About - BLTHunderstorm</title>
         </Head>
-        <div id="intro" className="box">
-          <div className="title">
+        <div id="intro" className={`${styles["box"]} box`}>
+          <div className={`${styles["title"]} title`}>
             <p id="service-name">BLThunderstorm</p>
-            <p className="message">
+            <p className={`${styles["message"]} message`}>
               Unofficial stats-tracking and dashboard site for Battlefield
               games.
             </p>
           </div>
         </div>
 
-        <div id="about" className="box">
-          <div id="features-container"></div>
+        <div id="about" className={`${styles["box"]} ${styles["about"]} box`}>
+          <div id={`${styles["features-container"]} features-container`}></div>
         </div>
       </div>
-    </DefaultLayout>
+  
   );
 }
