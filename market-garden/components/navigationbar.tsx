b8cfa0eca14e41a32766d2a8f613485da6726224
Nefomemes <nefothingy@hotmail.com>
2021-07-27 21:57:33 +0700

    feat(market-garden): Display NavigationBar

--- market-garden/components/navigationbar.tsx
+++ market-garden/components/navigationbar.tsx
@@ -3,11 +3,11 @@ import styles from "./NavigationBar.module.sass";
 
 export const NavigationBar: FunctionComponent = () => {
   return (
-    <div className={styles["navigation-bar-container"]}>
-      <div className="navigation-bar">
-        <div className="top-category-container">
-          <div className="nav-category home-category">
-            <div className="home-button button"></div>
+    <div className={`${styles["navigation-bar-container"]} navigation-bar-container`}>
+      <div className={`${styles["navigation-bar"]} navigation-bar`}>
+        <div className={`${styles["top-category-container"]} top-category-container`}>
+          <div className={`${styles["nav-category"]} ${styles["home-category"]} nav-category home-category`}>
+            <div className={`${styles["home-button"]} ${styles["button"]} home-button button`}></div>
           </div>
         </div>
         <div className="nav-category bottom-category">
